5e711630a27e7def88637bfefa4d3f25c91c0114
kiminokokoroni <artysh-man@mail.ru>
2022-07-24 20:40:11 +0700

    BeanPropertyWithId implementation

--- app/src/main/java/di/container/BeanPropertyWithId.java
+++ app/src/main/java/di/container/BeanPropertyWithId.java
@@ -1,25 +1,24 @@
 package di.container;
-
 public class BeanPropertyWithId implements BeanProperty {
     private BeanFactory parentFactory;
-
     private String id;
-
     private Class<?> clazz;
-
     @Override
     public String getId() {
         return id;
     }
-
     @Override
     public Object getBean() throws DIContainerException {
         return parentFactory.getBean(id);
     }
-
     @Override
     public Class<?> getClazz() {
         return clazz;
     }
 
+    public BeanPropertyWithId(BeanFactory parentFactory, String id, Class<?> clazz) {
+        this.parentFactory = parentFactory;
+        this.id = id;
+        this.clazz = clazz;
+    }
 }
