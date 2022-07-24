2b1e1e85a927c08d774c7172c8cc80845bb112cc
kiminokokoroni <artysh-man@mail.ru>
2022-07-24 13:15:44 +0700

    Work with constructor args

--- app/src/main/java/di/container/BeanDescription.java
+++ app/src/main/java/di/container/BeanDescription.java
@@ -1,42 +1,46 @@
 package di.container;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class BeanDescription {
     private BeanLifecycle beanLifecycle;
-
     private Class<?> clazz;
-
     private Object instance;
-
     private boolean isProxy;
-
     private final List<BeanProperty> constructorArgs = new ArrayList<>();
-
     private final List<BeanProperty> setterArgs = new ArrayList<>();
-
     public BeanLifecycle getBeanLifecycle() {
         return beanLifecycle;
     }
-
     public Class<?> getClazz() {
         return clazz;
     }
-
     public Object getInstance() {
         return instance;
     }
-
     public boolean isProxy() {
         return isProxy;
     }
-
     public List<BeanProperty> getConstructorArgs() {
         return constructorArgs;
     }
 
+    public void setInstance(Object instance) {
+        this.instance = instance;
+    }
+
     public List<BeanProperty> getSetterArgs() {
         return setterArgs;
+    }
+
+    public void setBeanLifecycle(BeanLifecycle beanLifecycle) {
+        this.beanLifecycle = beanLifecycle;
+    }
+
+    public void setClazz(Class<?> clazz) {
+        this.clazz = clazz;
+    }
+
+    public void setProxy(boolean proxy) {
+        isProxy = proxy;
     }
 }
--- app/src/main/java/di/container/JSONDIContainer.java
+++ app/src/main/java/di/container/JSONDIContainer.java
@@ -1,5 +1,4 @@
 package di.container;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,38 +6,48 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class JSONDIContainer implements DIContainer {
 
-    private final Map<String, BeanDescription> beans = new ConcurrentHashMap<>();
+    BeanFactory beanFactory = new BeanFactory();
+    // private final Map<String, BeanDescription> beans = new ConcurrentHashMap<>();
 
     @Override
     public <T> T getBean(String name, Class<T> clazz) throws DIContainerException {
-        return null;
+        Object bean = getBean(name);
+        if (clazz.isInstance(bean)) {
+            return (T)bean;
+        }
+        throw new DIContainerException(name + " is not " + clazz.getName());
+    }
+
+    public JSONDIContainer(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    public JSONDIContainer() {
+
     }
 
     @Override
     public Object getBean(String name) throws DIContainerException {
-        BeanDescription beanDescription = beans.get(name);
-        if (beanDescription == null) {
-            throw new DIContainerException("Illegal name!!!");
+   /* BeanDescription beanDescription = beans.get(name);
+    if (beanDescription == null) {
+      throw new DIContainerException("Illegal name!!!");
+    }
+    switch(beanDescription.getBeanLifecycle()) {
+      case SINGLETON:
+        if (beanDescription.getInstance() == null) {
+          //...
+        } else {
         }
-
-        switch(beanDescription.getBeanLifecycle()) {
-            case SINGLETON:
-                if (beanDescription.getInstance() == null) {
-                    //...
-                } else {
-                }
-                break;
-            case PROTOTYPE:
-                break;
-            case THREAD:
-                break;
-            default:
-                throw new DIContainerException("Unknown lifecycle");
-        }
-
-
-
-        return null;
+        break;
+      case PROTOTYPE:
+        break;
+      case THREAD:
+        break;
+      default:
+        throw new DIContainerException("Unknown lifecycle");
+    }
+*/
+        return beanFactory.getBean(name);
     }
 
     @Override
